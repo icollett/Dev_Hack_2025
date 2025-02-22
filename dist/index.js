5f6a36c91b5e98c7ec56f77e563a84dc8b7ab633
Liam <lcrosby@rrc.ca>
2025-02-21 20:55:18 -0600

    test for vercel take 2

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const chatbotRoutes_1 = __importDefault(require("./api/v1/routes/chatbotRoutes"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8080;
 app.get('/', (_req, res) => {
@@ -12,6 +13,7 @@ app.get('/', (_req, res) => {
 app.get('/ping', (_req, res) => {
     res.send('pong 🏓');
 });
+app.use('/api/v1/chatbot', chatbotRoutes_1.default);
 app.listen(port, () => {
     return console.log(`Server is listening on ${port}`);
 });
