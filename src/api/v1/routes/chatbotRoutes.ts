5f80eb01c11efb21056ef292fa52cbaad5efe720
Liam <lcrosby@rrc.ca>
2025-02-21 21:05:56 -0600

    added routes, controller, and service for chatbot

--- src/api/v1/routes/chatbotRoutes.ts
+++ src/api/v1/routes/chatbotRoutes.ts
@@ -1,15 +1,12 @@
 import express, { Response, Request, Router } from "express";
-//import * as chatbotController from "../controllers/chatbotController";
+import * as chatbotController from "../controllers/chatbotController";
 
 const router: Router = express.Router();
 
 
-//router.post("/", chatbotController.createMessage);
+router.post("/", chatbotController.createMessage);
 
-router.get("/", (req: Request, res: Response) => {
-	res.send("hello world")
-});
+router.get("/", chatbotController.getChatHistroy);
 
-//router.delete("/", chatbotController.deleteChatHistory);
+router.delete("/", chatbotController.deleteChatHistory);
 
-export default router;
